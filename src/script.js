a0ad70692d4b15bdda6ca390e0a1040b2bc07ae9
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-01-08 23:27:55 -0600

    Update script.js

--- src/script.js
+++ src/script.js
@@ -6,6 +6,7 @@ let psecond = document.querySelector('.second');
 
 // Function to update the time display
 function updateTime() {
+    console.log('Updating time...');
     let hourformat = dayjs().format('hh'); // 12-hour format
     let minute = dayjs().format('mm');    // Add leading zero
     let second = dayjs().format('ss');    // Add leading zero
